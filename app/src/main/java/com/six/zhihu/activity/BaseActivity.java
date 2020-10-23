471d71bcaa1813e7aedfe127c54aa14b865d5981
wangyou1024 <baiyuo1024@qq.com>
2020-10-23 20:11:52 +0800

    备注字体大小与着色

--- app/src/main/java/com/six/zhihu/activity/BaseActivity.java
+++ app/src/main/java/com/six/zhihu/activity/BaseActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// 建议实现基类
 public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
