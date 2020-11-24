68cb72f0f97dda3bc7260e5b4749085750196f27
yixingda <2014463023@qq.com>
2020-11-24 23:48:50 +0800

    完成我的创作中心、登录、关注页面、关注角色详细页面

---
+++ app/src/main/java/com/six/zhihu/activity/CreationActivity.java
@@ -0,0 +1,40 @@
+package com.six.zhihu.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.six.zhihu.R;
+
+public class CreationActivity extends BaseActivity {
+    private ImageView imageView;
+    @Override
+    protected int initLayout() {
+        return R.layout.activity_creation;
+    }
+
+    @Override
+    protected void initView() {
+        imageView = findViewById(R.id.creation_cancel);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Log.d("CreationActivity", "结束我的创作页面");
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
